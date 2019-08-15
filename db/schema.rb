05a04e7b6e92c906533be34af673d43f61b0b2cd
hiroki.yano <haraguros38@gmail.com>
2019-08-15 17:25:54 +0000

    user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_165043) do
+ActiveRecord::Schema.define(version: 2019_08_15_151520) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_08_12_165043) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
