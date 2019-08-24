c22ef17cbdd994108f9994ed0ac3313eb8d7d815
hiroki.yano <haraguros38@gmail.com>
2019-08-24 11:16:05 +0000

    tasklist & user connect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
       @tasks = Task.order(id: :desc).page(params[:page]).per(3)
       
-      if logged_in?
+        if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      end
+        end
   end
 
   def show
@@ -18,12 +19,13 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
